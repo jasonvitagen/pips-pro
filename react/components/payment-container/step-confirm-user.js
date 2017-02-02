01d9933c6584f005a366d6e05b542c0e56cd0c00
Jason Cheng <dz63@global.teso.org>
2017-02-03 00:42:05 +0800

    Content amendment

--- react/components/payment-container/step-confirm-user.js
+++ react/components/payment-container/step-confirm-user.js
@@ -6,8 +6,8 @@ export default class StepConfirmUser extends Component {
             <div className="col-md-4 col-sm-4 col-xs-12">
                 <div className="bg-white pinside40 number-block outline mb60 bg-boxshadow">
                     <div className="circle"><span className="number">2</span></div>
-                    <h3 className="number-title">Approved Your Loan</h3>
-                    <p>Fusce tempor sstibulum varius sem nec mi luctus viverra edcongue lobortis faucibus.</p>
+                    <h3 className="number-title">Choose Your Package</h3>
+                    <p></p>
                 </div>
             </div>
         );
--- react/components/payment-container/step-make-payment.js
+++ react/components/payment-container/step-make-payment.js
@@ -6,8 +6,8 @@ export default class StepConfirmUser extends Component {
             <div className="col-md-4 col-sm-4 col-xs-12">
                 <div className="bg-white pinside40 number-block outline mb60 bg-boxshadow">
                     <div className="circle"><span className="number">3</span></div>
-                    <h3 className="number-title">Get Your Cash</h3>
-                    <p>Get your money in minutes simtibulm varius semnec mluctus gue lobortis faucibus.</p>
+                    <h3 className="number-title">Make Payment</h3>
+                    <p></p>
                 </div>
             </div>
         );
