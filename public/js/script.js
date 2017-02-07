01cd8f90c7dad8d5b53b1f188e2f8fb0da6aba5f
Jason Cheng <dz63@global.teso.org>
2017-02-07 18:46:31 +0800

    Amended performance

--- public/js/script.js
+++ public/js/script.js
@@ -17,22 +17,24 @@ $(function () {
 
     $('.view-full-performance').click(function() {
         if ($('#view-full-performance').text() === 'View Full Performance Data') {
-            $('#view-full-performance').text('Hide Full Performance Data');
+            $('#view-full-performance').html($('#view-full-performance').html().replace('View Full Performance Data', 'Hide Full Performance Data'));
             $('#portfolio-filter').removeClass('hidden');
             $('#portfolio-results').removeClass('hidden');
-            $('#view-full-performance-2').removeClass('hidden');
+            setTimeout(function () {
+                $('#view-full-performance-2').removeClass('hidden');
+            });
             if (loaded) {
                 return $('#performance-chart').show();
             }
             loaded = true;
             addScript('/js/highcharts.js', 'highcharts');
             addScript('/js/view-performance-data.js', 'view-performance-data');
-            $('#view-full-performance-loader').addClass('ball-clip-rotate');
+            $('.view-full-performance-loader').addClass('ball-clip-rotate');
             $('#view-full-performance').attr('disabled', true);
         } else {
             $('#view-full-performance-2').addClass('hidden');
             $('#performance-chart').hide();
-            $('#view-full-performance').text('View Full Performance Data');
+            $('#view-full-performance').html($('#view-full-performance').html().replace('Hide Full Performance Data', 'View Full Performance Data'));
             $('#portfolio-filter').addClass('hidden');
             $('#portfolio-results').addClass('hidden');
             $('#performance-link').click();
