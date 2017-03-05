975b7da0a8d0a391dfea44dff8a1c325afec7136
Jason Cheng <dz63@global.teso.org>
2017-03-05 22:46:58 +0800

    payment

--- middlewares/check-payment-response-signature.js
+++ middlewares/check-payment-response-signature.js
@@ -4,7 +4,17 @@ const
 module.exports = (req, res, next) => {
     const {IPAY_MERCHANT_CODE, IPAY_MERCHANT_KEY} = process.env;
     const {MerchantCode, PaymentId, RefNo, Amount, Currency, Remark, TransId, AuthCode, Status, ErrDesc, Signature} = req.body;
-
+    console.log(MerchantCode);
+    console.log(PaymentId);
+    console.log(RefNo);
+    console.log(Amount);
+    console.log(Currency);
+    console.log(Remark);
+    console.log(TransId);
+    console.log(AuthCode);
+    console.log(Status);
+    console.log(ErrDesc);
+    console.log(Signature);
     const ourSignature = sha1(IPAY_MERCHANT_KEY + IPAY_MERCHANT_CODE + PaymentId + RefNo + Amount.split('.').join('') + Currency + Status);
     console.log(ourSignature);
 
