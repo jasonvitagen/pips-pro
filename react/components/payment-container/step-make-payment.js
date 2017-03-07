61a8dbe672c840fd0f0c8b491b1ef95d1388ac4b
Jason Cheng <dz63@global.teso.org>
2017-03-07 22:36:16 +0800

    Added non login check

--- react/components/payment-container/step-make-payment.js
+++ react/components/payment-container/step-make-payment.js
@@ -19,6 +19,7 @@ export default class StepConfirmUser extends Component {
     }
     render() {
         const {PaymentPostUrl, MerchantCode, PaymentId, RefNo, Amount, Currency, ProdDesc, UserName, UserEmail, UserContact, Remark, Signature, ResponseUrl, BackendUrl, verifying} = this.props.payment;
+        const disabled = Object.getOwnPropertyNames(this.props.user).length === 0 || verifying;
 
         let button;
         if (Object.getOwnPropertyNames(this.props.user).length === 0) {
@@ -39,67 +40,67 @@ export default class StepConfirmUser extends Component {
                         <table className="payment-method-list">
                         <tbody>
                         <tr>
-                            <td><input type="radio" id="cimb" name="payment-method" value="20" onChange={this.paymentIdChanged.bind(this, 20)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="cimb" name="payment-method" value="20" onChange={this.paymentIdChanged.bind(this, 20)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="cimb" className="payment-label payment-cimb"></label></td>
                         </tr>
                         <tr>
-                            <td><input type="radio" id="maybank" name="payment-method" value="6" onChange={this.paymentIdChanged.bind(this, 6)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="maybank" name="payment-method" value="6" onChange={this.paymentIdChanged.bind(this, 6)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="maybank" className="payment-label payment-maybank"></label></td>
                         </tr>
                         <tr>
-                            <td><input type="radio" id="hongleong" name="payment-method" value="15" onChange={this.paymentIdChanged.bind(this, 15)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="hongleong" name="payment-method" value="15" onChange={this.paymentIdChanged.bind(this, 15)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="hongleong" className="payment-label payment-hongleong"></label></td>
                         </tr>
                         <tr>
-                            <td><input type="radio" id="ambank" name="payment-method" value="10" onChange={this.paymentIdChanged.bind(this, 10)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="ambank" name="payment-method" value="10" onChange={this.paymentIdChanged.bind(this, 10)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="ambank" className="payment-label payment-ambank"></label></td>
                         </tr>
                         <tr>
-                            <td><input type="radio" id="alliance" name="payment-method" value="8" onChange={this.paymentIdChanged.bind(this, 8)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="alliance" name="payment-method" value="8" onChange={this.paymentIdChanged.bind(this, 8)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="alliance" className="payment-label payment-alliance"></label></td>
                         </tr>
                         <tr>
-                            <td><input type="radio" id="rhb" name="payment-method" value="14" onChange={this.paymentIdChanged.bind(this, 14)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="rhb" name="payment-method" value="14" onChange={this.paymentIdChanged.bind(this, 14)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="rhb" className="payment-label payment-rhb"></label></td>
                         </tr>
                         <tr>
-                            <td><input type="radio" id="islam" name="payment-method" value="134" onChange={this.paymentIdChanged.bind(this, 134)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="islam" name="payment-method" value="134" onChange={this.paymentIdChanged.bind(this, 134)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="islam" className="payment-label payment-islam"></label></td>
                         </tr>
                         <tr>
-                            <td><input type="radio" id="publicbank" name="payment-method" value="31" onChange={this.paymentIdChanged.bind(this, 31)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="publicbank" name="payment-method" value="31" onChange={this.paymentIdChanged.bind(this, 31)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="publicbank" className="payment-label payment-publicbank"></label></td>
                         </tr>
                         <tr>
-                            <td><input type="radio" id="fpx" name="payment-method" value="16" onChange={this.paymentIdChanged.bind(this, 16)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="fpx" name="payment-method" value="16" onChange={this.paymentIdChanged.bind(this, 16)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="fpx" className="payment-label payment-fpx"></label></td>
                         </tr>
                         <tr>
-                            <td><input type="radio" id="webcash" name="payment-method" value="22" onChange={this.paymentIdChanged.bind(this, 22)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="webcash" name="payment-method" value="22" onChange={this.paymentIdChanged.bind(this, 22)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="webcash" className="payment-label payment-webcash"></label></td>
                         </tr>
                         <tr>
-                            <td><input type="radio" id="affinbank" name="payment-method" value="103" onChange={this.paymentIdChanged.bind(this, 103)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="affinbank" name="payment-method" value="103" onChange={this.paymentIdChanged.bind(this, 103)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="affinbank" className="payment-label payment-affinbank"></label></td>
                         </tr>
                         <tr>
-                            <td><input type="radio" id="uob" name="payment-method" value="152" onChange={this.paymentIdChanged.bind(this, 152)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="uob" name="payment-method" value="152" onChange={this.paymentIdChanged.bind(this, 152)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="uob" className="payment-label payment-uob"></label></td>
                         </tr>
                         <tr>
-                            <td><input type="radio" id="standardchartered" name="payment-method" value="168" onChange={this.paymentIdChanged.bind(this, 168)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="standardchartered" name="payment-method" value="168" onChange={this.paymentIdChanged.bind(this, 168)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="standardchartered" className="payment-label payment-standardchartered"></label></td>
                         </tr>
                         <tr>
-                            <td><input type="radio" id="ocbc" name="payment-method" value="167" onChange={this.paymentIdChanged.bind(this, 167)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="ocbc" name="payment-method" value="167" onChange={this.paymentIdChanged.bind(this, 167)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="ocbc" className="payment-label payment-ocbc"></label></td>
                         </tr>
                         <tr>
-                            <td><input type="radio" id="muamalat" name="payment-method" value="166" onChange={this.paymentIdChanged.bind(this, 166)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="muamalat" name="payment-method" value="166" onChange={this.paymentIdChanged.bind(this, 166)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="muamalat" className="payment-label payment-muamalat"></label></td>
                         </tr>
                         <tr>
-                            <td><input type="radio" id="bsn" name="payment-method" value="124" onChange={this.paymentIdChanged.bind(this, 124)} className="payment-method-radio" disabled={verifying} /></td>
+                            <td><input type="radio" id="bsn" name="payment-method" value="124" onChange={this.paymentIdChanged.bind(this, 124)} className="payment-method-radio" disabled={disabled} /></td>
                             <td><label htmlFor="bsn" className="payment-label payment-bsn"></label></td>
                         </tr>
                         </tbody>
