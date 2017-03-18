f04a09c29a9be93c7f502211ac187bd96b6c865a
Jason Cheng <dz63@global.teso.org>
2017-03-19 05:47:34 +0800

    mongo index

--- deployment/mongo.setupIndices.js
+++ deployment/mongo.setupIndices.js
@@ -1,3 +1,9 @@
 const
     mongo = new Mongo()
     , db = mongo.getDB('pipspro');
+
+db.User.dropIndexes();
+db.User.createIndex({ 'email' : 1 }, { unique : true });
+
+db.Transaction.dropIndexes();
+db.Transaction.createIndex({ 'UserEmail' : 1 });
