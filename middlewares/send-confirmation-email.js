f681c1e2dc604c9c19cad09111d707cf96b2c5b8
Jason Cheng <dz63@global.teso.org>
2017-03-20 00:12:21 +0800

    Confirmation email

--- middlewares/send-confirmation-email.js
+++ middlewares/send-confirmation-email.js
@@ -1,5 +1,6 @@
 const
-    mailgun = require('../setup/mailgun');
+    mailgun = require('../setup/mailgun')
+    , emailTemplate = require('../emails/confirmation');
 
 module.exports = (req, res, next) => {
     process.nextTick(() => {
@@ -9,15 +10,7 @@ module.exports = (req, res, next) => {
                 from: 'Pips-Pro <admin@pips-pro.com>',
                 to: req.body.email,
                 subject: 'Registration at Pips-Pro.com',
-                html: `
-                    <p>Hi ${name},</p>
-                    <p>Thank you for your registration at pips-pro.com</p>
-                    <p>Please find below your registration info:</p>
-                    <p>Email: ${email}</p>
-                    <p>Mobile: ${mobile}</p>
-                    <p>Thanks,</p>
-                    <p>Pips-Pro</p>
-                `
+                html: emailTemplate(req.user)
             };
 
         mailgun.messages().send(message, (err, body) => {
