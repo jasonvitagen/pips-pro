9d9a74a80d8cec59e7774cdf4af27d0b51362d6b
Jason Cheng <dz63@global.teso.org>
2017-05-17 22:54:29 +0800

    notify expired users

--- setup/notify-expired-users.js
+++ setup/notify-expired-users.js
@@ -4,26 +4,25 @@ const
     , db = require('./mongo')
     , cron = require('node-cron');
 
-cron.schedule('*/5 * * * * *', () => {
+cron.schedule('0 14 * * *', () => {
+    console.log('Running notify expired users cron task');
     db.User.find({packageExpireAt: {$lt: new Date(), $gt: new Date(new Date().setDate(new Date().getDate() - 30))}}, (err, users) => {
         if (err) {
             return console.log(err);
         }
         users.forEach(user => {
-            if (user.email === 'jasoncheng@live.com.my') {
-                const message = {
-                    from: 'Pips-Pro <admin@pips-pro.com>',
-                    to: user.email,
-                    subject: 'Signal Package Expired at Pips-Pro.com',
-                    html: emailTemplate(user)
-                };
-                mailgun.messages().send(message, (err, body) => {
-                    if (err) {
-                        console.log(err);
-                    }
-                    console.log(body);
-                });
-            }
+            const message = {
+                from: 'Pips-Pro <admin@pips-pro.com>',
+                to: user.email,
+                subject: 'Signal Package Expired at Pips-Pro.com',
+                html: emailTemplate(user)
+            };
+            mailgun.messages().send(message, (err, body) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log(body);
+            });
         });
     });
 });
